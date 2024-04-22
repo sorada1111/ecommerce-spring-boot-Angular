ee0ec5b3d59d722a78caff7c7f2682141c38fbcc
sorada1111 <sorada1111@gmail.com>
2024-04-22 19:54:28 -0400

    first commit

---
+++ src/main/java/com/sp/ecommerce/dao/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.sp.ecommerce.dao;
+
+import com.sp.ecommerce.entity.Product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhost:4200")
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
