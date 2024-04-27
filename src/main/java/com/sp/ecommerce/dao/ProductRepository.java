4f3a820b2e9ec142ce9a1a98ff0ca89f89ec079e
sorada1111 <sorada1111@gmail.com>
2024-04-27 14:56:15 -0400

    Add new search method for search product by keyword, Closes #9

--- src/main/java/com/sp/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/sp/ecommerce/dao/ProductRepository.java
@@ -11,7 +11,13 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long>{
 	Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
+	Page<Product> findByNameContaining(@Param("name") String name, Pageable page);
 }
 
 
 // findByCategoryId  === select * from products where category_id =?
+// http://localhost:8080/api/products/search/findByCategoryId?id=1
+
+
+//findByNameContaining  === select * from products p where p.name LIKE CONCAT('%', :name,'%')
+//http://localhost:8080/api/products/search/findByNameContaining?name=Python
