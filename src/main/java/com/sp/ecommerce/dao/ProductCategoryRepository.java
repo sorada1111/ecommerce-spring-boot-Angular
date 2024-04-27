74e8b5660a1dc191291c93670275196f28851541
sorada1111 <sorada1111@gmail.com>
2024-04-27 17:58:47 -0400

    Update CORS for all site

--- src/main/java/com/sp/ecommerce/dao/ProductCategoryRepository.java
+++ src/main/java/com/sp/ecommerce/dao/ProductCategoryRepository.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import com.sp.ecommerce.entity.ProductCategory;
 
 
-@CrossOrigin("http://localhost:4200")
+@CrossOrigin
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
